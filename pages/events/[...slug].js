5486865b04e68bf1f555588f46076eb91e61e6b0
春日崇希 <kasugatakashinozomi@kasugatakashinozominoMacBook-Pro.local>
2021-07-26 16:53:56 +0900

    10-lesson完了

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -16,7 +16,7 @@ function FilteredEventsPage(props) {
   const filterData = router.query.slug;
 
   const { data, error } = useSWR(
-    'https://nextjs-course-c81cc-default-rtdb.firebaseio.com/events.json'
+    'https://nextjs-course-3645a-default-rtdb.firebaseio.com/events.json'
   );
 
   useEffect(() => {
@@ -26,7 +26,7 @@ function FilteredEventsPage(props) {
       for (const key in data) {
         events.push({
           id: key,
-          ...data[key],
+          ...data[key]
         });
       }
 
