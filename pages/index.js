55e16a2d649a2620eabb4baa1ba7aa7cff09a6a1
春日崇希 <kasugatakashinozomi@kasugatakashinozominoMacBook-Pro.local>
2021-07-26 09:37:52 +0900

    Finished Backend-API-Routes

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,60 @@
+import { useRef, useState } from 'react';
+
 function HomePage() {
+  const emailInputref = useRef();
+  const feedbackInputref = useRef();
+  const [feedbackItems, setFeedbackItems] = useState([]);
+
+  function submitFormHandler(event) {
+    event.preventDefault();
+
+    const enterdEmail = emailInputref.current.value;
+    const enterdFeedback = feedbackInputref.current.value;
+
+    const reqBody = { email: enterdEmail, text: enterdFeedback };
+
+    fetch('/api/feedback', {
+      method: 'POST',
+      body: JSON.stringify(reqBody),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((response) => {
+        response.json();
+      })
+      .then((data) => {
+        console.log('data', data);
+      });
+  }
+
+  function loadFeedbackHandler() {
+    fetch('/api/feedback')
+      .then((response) => response.json())
+      .then((data) => {
+        setFeedbackItems(data.feedback);
+      });
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor='email'>Your Email Address</label>
+          <input type='email' id='email' ref={emailInputref} />
+        </div>
+        <div>
+          <label htmlFor='feedback'>Your Feedback</label>
+          <textarea id='feedback' rows='5' ref={feedbackInputref}></textarea>
+        </div>
+        <button>Send Feedback</button>
+      </form>
+      <hr />
+      <button onClick={loadFeedbackHandler}>Load Feedback</button>
+      {feedbackItems.map((feedbackItem) => {
+        return <li key={feedbackItem.id}>{feedbackItem.text}</li>;
+      })}
     </div>
   );
 }
